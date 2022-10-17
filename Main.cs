65c5f4280335acf34793dc0d0c04a61a4b8054d2
Gulzhanat <110971682+gulzh@users.noreply.github.com>
2022-10-17 17:26:57 +0200

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using System;
+using UnityEngine.EventSystems;
+
+public class Main : MonoBehaviour
+{
+  
+    
+    
+
+
+    public void CreateText()
+    {
+
+
+        //Check for Flip/Unflip
+        string[] config = File.ReadAllLines(Application.dataPath + "/Logs/UserConfig/Config.txt");
+
+  
+
+        //File for Training
+
+        string directoryPathTraining = Application.dataPath + "/Logs/ShuffleList/Training";
+
+        if (!Directory.Exists(directoryPathTraining))
+        {
+            Directory.CreateDirectory(Application.dataPath + "/Logs/ShuffleList/Training");
+        }
+
+
+        DirectoryInfo dirTrain = new DirectoryInfo(Application.dataPath + "/Logs/ShuffleList/Training");
+        
+        string user = config[4];
+
+        string txtDocumentNameTrain = dirTrain + "/Log_User_" + user + ".txt";
+        if (File.Exists(txtDocumentNameTrain))
+        {
+            Console.WriteLine("The file already exists!");
+
+        }
+
+        File.WriteAllText(txtDocumentNameTrain, "Training session" + "\n\n" + "Logs for User" + user +  "\n\n");
+        File.AppendAllText(txtDocumentNameTrain, "Name: " + config[0]  +  "\n\n");
+        File.AppendAllText(txtDocumentNameTrain, "Surname: " + config[1]  +  "\n\n");
+        File.AppendAllText(txtDocumentNameTrain, "Age: " + config[2]  + "\n\n");
+        File.AppendAllText(txtDocumentNameTrain, "Sex: " + config[3]  + "\n\n");
+        File.AppendAllText(txtDocumentNameTrain, "Login date: " + System.DateTime.Now + "\n\n");
+  
+
+
+
+        File.WriteAllText(Application.dataPath + "/Logs/Recording_" + user + ".txt", "");
+
+
+
+
+
+
+        //File for Testing
+        string directoryPathTesting = Application.dataPath + "/Logs/ShuffleList/Testing";
+
+        if (!Directory.Exists(directoryPathTesting))
+        {
+            Directory.CreateDirectory( Application.dataPath + "/Logs/ShuffleList/Testing");
+        }
+
+
+        DirectoryInfo dirTest = new DirectoryInfo( Application.dataPath + "/Logs/ShuffleList/Testing");
+        
+        //FileInfo[] filesTest = dirTest.GetFiles();
+        //int userTest = filesTest.Length;
+
+        string txtDocumentNameTest = dirTest + "/Log_User_" + user + ".txt";
+        if (File.Exists(txtDocumentNameTest))
+        {
+            Console.WriteLine("The file already exists!");
+
+        }
+
+        File.WriteAllText(txtDocumentNameTest, "Testing session" + "\n\n"+ "Logs for User" + user + "\n\n");
+        File.AppendAllText(txtDocumentNameTest, "Login date: " + System.DateTime.Now + "\n\n");
+        File.AppendAllText(txtDocumentNameTest, "Name: " + config[0] +  "\n\n");
+        File.AppendAllText(txtDocumentNameTest, "Surname: " + config[1] +  "\n\n");
+        File.AppendAllText(txtDocumentNameTest, "Age: " + config[2]  +  "\n\n");
+        File.AppendAllText(txtDocumentNameTest, "Sex: " + config[3]  +  "\n\n");
+
+
+    }
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        CreateText();
+        
+       
+        
+    }
+
+
+}
---
+++ ShuffleListTesting.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using System.IO;
+using UnityEngine;
+
+public class ShuffleListTesting : MonoBehaviour
+{
+
+
+    public string[] filesList;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        CreateList();
+        //Meshes = Resources.LoadAll("Carlos_Testing/" + initialFramePc.Split('_')[0] + "/",typeof(GameObject)) as GameObject[];
+    }
+
+
+    public void CreateList()
+    {
+        string[] config = File.ReadAllLines( Application.dataPath + "/Logs/UserConfig/Config.txt");
+        string user = config[4];
+
+        string InitialFrameFolder = GameObject.Find("NonDestructibleObject").GetComponent<InitialFrameScript>().InitialFrame;
+        string sourcePath = Application.dataPath + "/Resources/PointCloud/"+InitialFrameFolder+"/";
+        DirectoryInfo m_Path = new DirectoryInfo(sourcePath);
+        FileInfo[] files = m_Path.GetFiles("*.ply");
+        string[] filesName = new string[files.Length];
+        int i = 0;
+
+        foreach (FileInfo file in files)
+        {
+
+            string fileName = file.Name.Replace(file.Extension, "");
+
+
+            filesName[i] = (fileName);
+
+            i++;
+        }
+
+        filesList = ReshuffleList(filesName);
+        int p = 0;
+        File.WriteAllText( Application.dataPath + "/Logs/ShuffleList/Testing/ShuffleList_" + user + ".txt", "");
+
+        foreach (string element in this.filesList)
+        {
+            File.AppendAllText(Application.dataPath + "/Logs/ShuffleList/Testing/ShuffleList_" + user +".txt", this.filesList[p] + "\n");
+            p++;
+        }
+
+
+    }
+    private void Update()
+    {
+        
+
+    }
+
+    private string[] ReshuffleList(string[] texts)
+    {
+        for (int t = 0; t < texts.Length; t++)
+        {
+            string tmp = texts[t];
+            int r = Random.Range(t, texts.Length);
+            texts[t] = texts[r];
+            texts[r] = tmp;
+        }
+        return texts;
+    }
+
+
+
+}
